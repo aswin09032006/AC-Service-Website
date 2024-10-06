0eb1af6a3a50051460901d19804a4da4afdabdd4
ASWIN V K <vkaswinkanan@gmail.com>
2024-10-06 17:09:45 +0530

    index update

--- acservice/index.js
+++ acservice/index.js
@@ -5,29 +5,11 @@ import simpleGit from 'simple-git';
 const filePath = './data.json';
 const git = simpleGit();
 
-// Helper function to generate an array of dates between a start and end date
-const getDateRange = (startDate, endDate) => {
-  const start = moment(startDate);
-  const end = moment(endDate);
-  const dates = [];
+// Generate the past 15 days
+const past15Days = Array.from({ length: 35 }, (_, i) => moment().subtract(i + 1, 'days').format());
 
-  while (start.isSameOrBefore(end)) {
-    dates.push(start.format()); // Add each date to the array
-    start.add(1, 'd'); // Increment by one day
-  }
-
-  return dates;
-};
-
-// Generate dates for Oct 2-4 and Sep 4-29
-const octDates = getDateRange('2023-10-02', '2023-10-04');
-const sepDates = getDateRange('2023-09-04', '2023-09-29');
-
-// Combine all dates
-const allDates = [...octDates, ...sepDates];
-
-async function commitOnDates() {
-  for (const dateStr of allDates) {
+async function commitForPast15Days() {
+  for (const dateStr of past15Days) {
     const DATE = moment(dateStr).format(); // Format each date
 
     // Add data to JSON file with the current date
@@ -42,7 +24,3 @@ async function commitOnDates() {
   // Push all the commits after the loop
   await git.push();
 }
-
-commitOnDates().then(() => {
-  console.log('All commits pushed on specified dates.');
-});
